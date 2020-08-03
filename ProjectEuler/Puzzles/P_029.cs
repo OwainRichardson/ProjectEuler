9f7c0959a5f701dda1c93a81d9f20ddd72b53a37
Owain Richardson <owain.richardson@gmail.com>
2020-08-03 15:28:36 +0100

    Do some more projects and update .net core version

---
+++ ProjectEuler/Puzzles/P_029.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace ProjectEuler.Puzzles
+{
+    public static class P_029
+    {
+        public static void Execute()
+        {
+            int start = 2;
+            int end = 100;
+            List<BigInteger> terms = new List<BigInteger>();
+
+            for (int a = start; a <= end; a++)
+            {
+                for (int b = start; b <= end; b++)
+                {
+                    BigInteger value = (BigInteger)Math.Pow(a, b);
+
+                    if (!terms.Contains(value))
+                    {
+                        terms.Add(value);
+                    }
+                }
+            }
+
+            Console.WriteLine(terms.Distinct().Count());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ProjectEuler/Puzzles/P_030.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace ProjectEuler.Puzzles
+{
+    public static class P_030
+    {
+        public static void Execute()
+        {
+            int start = 2;
+            int end = 999999;
+            List<int> successfullCases = new List<int>();
+
+            for (int val = start; val <= end; val++)
+            {
+                long total = 0;
+                string textNumber = val.ToString();
+
+                for (int index = 0; index < textNumber.Length; index++)
+                {
+                    total += (long)Math.Pow(int.Parse(textNumber[index].ToString()), 5);
+
+                    if (total > val)
+                    {
+                        break;
+                    }
+                }
+
+                if (total == val)
+                {
+                    successfullCases.Add(val);
+                }
+            }
+
+            Console.Write(successfullCases.Sum());
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
